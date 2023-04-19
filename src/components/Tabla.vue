d2e3208282a2a50fd6071d90ea261e51f0783bbb
Richard <e18184@gmail.com>
2023-04-19 15:47:09 -0400

    19-4-2023

---
+++ src/components/Tabla.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+    Esta es nuestra tabla 
+    Usuarios*
+    {{usuario}}
+
+    </div>
+</template>
+<script>
+export default {
+    name: 'Tabla',
+    data() {
+        return {
+            usuario: []
+
+        }
+        
+    },
+    methods : {
+        getUsuario(){
+      fetch('https://my-json-server.typicode.com/joseluisgs/APIRESTFake/photos')
+      .then(response=> response.json())
+      .then(data=>{
+        this.usuario = data
+    })
+}  
+}
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Tabla from './components/Tabla.vue'
 
 Vue.config.productionTip = false
-
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+const routes = [
+  /* cada de declaracion es una ruta */
+  {
+    path: '/',
+    name: 'Vue',
+    component: Vue
+  }
+  ,{
+    path: '/tabla',
+    name: 'Tabla',
+    component: Tabla 
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
